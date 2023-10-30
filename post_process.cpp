cd27e8683a4fed6e34d2f7cc69c487ad1f5e114b
Salvatore Logozzo <sal.logozzo@gmail.com>
2023-10-30 17:28:03 -0400

    Created functions to calculate zero-crossing rate, hjorth parameters, and STFT

--- post_process.cpp
+++ post_process.cpp
@@ -10,6 +10,13 @@
 
 using namespace std;
 
+// Create struct for Hjorth parameters
+struct HjorthParameters {
+    double activity;
+    double mobility;
+    double complexity;
+};
+
 // Function to compute mean of the signal
 double computeMean(const std::vector<double>& signalData) {
     double sum = 0.0;
@@ -54,24 +61,24 @@ double computeKurtosis(const std::vector<double>& signalData, double mean, doubl
 }
 
 // Function to compute FFTW of the signal
-std::vector<std::complex<double>> computeFFTW(const std::vector<double>& signalData) {
-    int N = signalData.size();
+std::vector<std::complex<double>> computeFFTW(const std::vector<double>& signalData, int N) {
     fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
     fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
 
     fftw_plan plan = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 
-    for (int i = 0; i < N; i++) {
+    for (int i=0;i<N;i++) {
         in[i][0] = signalData[i]; // Real part
         in[i][1] = 0.0;           // Imaginary part (set to 0 for real data)
     }
 
+    // Perform FFTW
     fftw_execute(plan);
 
     std::vector<std::complex<double>> fftwResult;
     fftwResult.reserve(N);
 
-    for (int i = 0; i < N; i++) {
+    for (int i=0;i<N;i++) {
         std::complex<double> complexValue(out[i][0], out[i][1]);
         fftwResult.push_back(complexValue);
     }
@@ -121,9 +128,7 @@ double calculateRelativePower(std::vector<std::complex<double>> fftwResult, doub
 }
 
 // Function to calculate Shannon entropy
-double calculateShannonEntropy(const std::vector<double>& signalData, int numBins) {
-    int dataSize = signalData.size();
-
+double calculateShannonEntropy(const std::vector<double>& signalData, int numBins, int N) {
     // Calculate min and max values in the signal data
     double minVal = *std::min_element(signalData.begin(), signalData.end());
     double maxVal = *std::max_element(signalData.begin(), signalData.end());
@@ -135,7 +140,7 @@ double calculateShannonEntropy(const std::vector<double>& signalData, int numBin
     std::map<int, int> histogram;
 
     // Populate the histogram
-    for (int i = 0; i < dataSize; i++) {
+    for (int i=0;i<N;i++) {
         int binIndex = static_cast<int>((signalData[i] - minVal) / binWidth);
         histogram[binIndex]++;
     }
@@ -143,13 +148,94 @@ double calculateShannonEntropy(const std::vector<double>& signalData, int numBin
     // Calculate probability distribution and Shannon entropy
     double entropy = 0.0;
     for (const auto& entry : histogram) {
-        double probability = static_cast<double>(entry.second) / dataSize;
+        double probability = static_cast<double>(entry.second) / N;
         if (probability > 0.0) {
             entropy -= probability * log2(probability);
         }
     }
 
     return entropy;
+}
+
+// Function to calculate zero-crossing rate
+int calculateZeroCrossingRate(const std::vector<double>& signalData, int N) {
+    int zeroCrossings = 0;
+    for (int i=1;i<N;i++) {
+        // Check if the current value has a different sign than the previous value
+        if ((signalData[i] >= 0 && signalData[i - 1] < 0) || (signalData[i] < 0 && signalData[i - 1] >= 0)) {
+            zeroCrossings++;
+        }
+    }
+    return zeroCrossings;
+}
+
+// Function to calculate Hjorth parameters
+HjorthParameters calculateHjorthParameters(const std::vector<double>& signalData, int N) {
+    HjorthParameters hjorth;
+
+    // Calculate Activity
+    double activity = 0.0;
+    for (int i=0;i<N;i++) {
+        activity += signalData[i] * signalData[i];
+    }
+    hjorth.activity = activity / N;
+
+    // Calculate Mobility
+    double mobility = 0.0;
+    for (int i=0;i<N;i++) {
+        mobility += pow(signalData[i] - signalData[i - 1], 2);
+    }
+    mobility = sqrt(mobility / (N - 1)) / sqrt(hjorth.activity);
+    hjorth.mobility = mobility;
+
+    // Calculate Complexity
+    double complexity = 0.0;
+    for (int i=0;i<N;i++) {
+        complexity += pow(signalData[i] - 2 * signalData[i - 1] + signalData[i - 2], 2);
+    }
+    complexity = sqrt(complexity / (N - 1)) / mobility;
+    hjorth.complexity = complexity;
+
+    return hjorth;
+}
+
+// Function to perform Short-Time Fourier Transform (STFT)
+std::vector<std::vector<std::complex<double>>> calculateSTFT(const std::vector<double>& signalData, int windowSize, int hopSize, int N) {
+    std::vector<std::vector<std::complex<double>>> stftResult;
+
+    // Initialize FFTW plans
+    fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * windowSize);
+    fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * windowSize);
+    fftw_plan plan = fftw_plan_dft_1d(windowSize, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    int numFrames = (N - windowSize) / hopSize + 1;
+
+    for (int frame=0;frame<numFrames;frame++) {
+        std::vector<std::complex<double>> stftFrame;
+
+        // Fill the input buffer with the current frame
+        for (int i=0;i<windowSize;i++) {
+            in[i][0] = signalData[frame * hopSize + i];
+            in[i][1] = 0.0; // Imaginary part is zero
+        }
+
+        // Perform FFT
+        fftw_execute(plan);
+
+        // Copy FFT result to the STFT frame
+        for (int i=0;i<windowSize;i++) {
+            stftFrame.push_back(std::complex<double>(out[i][0], out[i][1]));
+        }
+
+        stftResult.push_back(stftFrame);
+    }
+
+    // Clean up FFTW resources
+    fftw_destroy_plan(plan);
+    fftw_free(in);
+    fftw_free(out);
+
+    return stftResult;
 }
 
 int main() {
@@ -190,6 +276,9 @@ int main() {
     double muMax = 12.0; // Upper bound of mu band (Hz)
     // Define the number of bins for discretization (for Shannon entropy)
     int numBins = 10; // This value can be adjusted as needed
+    // Define some STFT parameters
+    int windowSize = 256; // This value can be adjusted as needed
+    int hopSize = 64; // This value can be adjusted as needed
 
     // Calculate the mean
     double mean = computeMean(signalData);
@@ -200,7 +289,7 @@ int main() {
     // Calculate kurtosis
     double kurtosis = computeKurtosis(signalData, mean, variance);
     // Calculate FFTW and get the result
-    std::vector<std::complex<double>> fftwResult = computeFFTW(signalData);
+    std::vector<std::complex<double>> fftwResult = computeFFTW(signalData, N);
     // Calculate relative delta power
     double relativeDeltaPower = calculateRelativePower(fftwResult, deltaMin, deltaMax, N);
     // Calculate relative theta power
@@ -214,7 +303,13 @@ int main() {
     // Calculate relative mu power
     double relativeMuPower = calculateRelativePower(fftwResult, muMin, muMax, N);
     // Calculate the Shannon entropy
-    double entropy = calculateShannonEntropy(signalData, numBins);
+    double entropy = calculateShannonEntropy(signalData, numBins, N);
+    // Calculate the zero-crossing rate
+    int zeroCrossingRate = calculateZeroCrossingRate(signalData, N);
+    // Calculate Hjorth parameters
+    HjorthParameters params = calculateHjorthParameters(signalData, N);
+    // Calculate STFT and get the result
+    std::vector<std::vector<std::complex<double>>> stftResult = calculateSTFT(signalData, windowSize, hopSize, N);
 
     // Debug: Print all values
     std::cout << "Mean of the signal: " << mean << std::endl;
@@ -229,6 +324,8 @@ int main() {
     std::cout << "Relative Gamma Power: " << relativeGammaPower << std::endl;
     std::cout << "Relative Mu Power: " << relativeMuPower << std::endl;
     std::cout << "Shannon Entropy: " << entropy << std::endl;
+    std::cout << "Zero-Crossing Rate: " << zeroCrossingRate << std::endl;
+    std::cout << "Hjorth parameters - Activity: " << params.activity << " - Mobility: " << params.mobility << " - Complexity: " << params.complexity << std::endl;
 
     return 0;
     
