9b1dcd90fd403c29aa9b7468309c402ed5edd855
Salvatore Logozzo <sal.logozzo@gmail.com>
2024-01-22 19:06:05 -0500

    Modified filter to be a bandpass filter

--- clean_and_preprocess.m
+++ clean_and_preprocess.m
@@ -4,7 +4,7 @@ function [data_filtered, num_peaks] = clean_and_preprocess(csv)
     data = normalize(data,"range"); % min-max normalization
     Fs = 512; % Will become 512Hz once working with data coming from the actual headset.
     % Design low pass filter and filter signal
-    digFilt = designfilt('lowpassiir','PassbandFrequency',8,'StopbandFrequency',30,'PassbandRipple',1,'StopbandAttenuation',60,'SampleRate',Fs);
+    digFilt = designfilt('bandpassiir','FilterOrder', 4,'HalfPowerFrequency1', mu_band(1),'HalfPowerFrequency2', beta_band(2),'SampleRate', fs);
     % Filtering data and Compensating for the delay
     data_filtered = filtfilt(digFilt, data);
     num_peaks = numel(findpeaks(data_filtered, Fs));
